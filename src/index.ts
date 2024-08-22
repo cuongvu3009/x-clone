96e0b2850106d28b623dac12627b220abb8ca073
cuongvu3009 <cuongvu3009@gmail.com>
2024-08-23 01:30:45 +0300

    feat: connect db

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-const name: string = 'Cuong'
-console.log(name)
+require('dotenv').config()
+import express from 'express'
+import databaseService from './services/database.services'
+import userRouter from './routes/users.routes'
+
+const app = express()
+const port = process.env.PORT
+
+//  Database connection
+databaseService.connect()
+
+app.get('/', (req, res ) => {
+    res.send("Hello World")
+})
+app.use('/api', userRouter)
+
+
+app.listen(port, () => console.log(`Server is listening on ${port}`))
